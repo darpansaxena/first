4bbab12c310dbc0acece7e647a4cca6bbcdc21a6
darpansaxena <13.darpan@gmail.com>
2016-06-29 20:51:07 +0530

    first commit

---
+++ answer.pseudo.php
@@ -0,0 +1,14 @@
+<?php
+    function first($name)  //function  for name. 
+    {
+  	echo $name; //produce name.
+    }
+    function second($int)//function for random number.
+    {
+  	$int = rand(6, 15);
+	echo $int;
+    }
+    first("shrey");  //calling of name function .
+    second(6, 15);  //calling of number function.
+?>
+    
